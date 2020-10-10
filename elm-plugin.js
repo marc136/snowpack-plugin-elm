78ac33ca9e4469614c11b28596ff1fedc95a2629
Marc Walter <walter.marc@outlook.com>
2020-10-10 11:04:38 +0200

    Initial working version of an Elm plugin
    What it can do:
    * Compile an imported Elm file to an ES module
    * When the imported file is changed, the browser reloads the elm code
    
    What it cannot do:
    * Trigger an update if an imported Elm file changes
    * Keep the Elm app state when replacing with a new version
    * Create an optimized version

---
+++ elm-plugin.js
@@ -0,0 +1,56 @@
+const fs = require('fs').promises;
+const path = require('path');
+const elm = require('node-elm-compiler');
+const elmHot = require('elm-hot');
+
+module.exports = (snowpackConfig, userPluginOptions) => {
+  return {
+    name: 'elm-plugin',
+
+    resolve: { input: ['.elm'], output: ['.js'] },
+
+    config(snowpackConfig) {
+      // snowpack config was built and can be accessed here
+      console.warn('elm-plugin.config()');
+    },
+
+    async load({ fileExt, filePath, isDev, isHmrEnabled }) {
+      const args = { fileExt, filePath, isDev, isHmrEnabled };
+      console.warn('elm-plugin.load()', args);
+      try {
+        // TODO check isHmrEnabled and use the inject function from elm-hot
+
+        const iife = await elm.compileToString([filePath], {
+          debug: isDev,
+          optimize: !isDev,
+        });
+        return toESM(iife);
+      } catch (err) {
+        console.error('ERROR:', err.message || err);
+        throw err;
+      }
+    },
+
+    async onchange({ filePath }) {
+      // TODO use this to check Elm module dependencies and support HMR for imported modules
+      console.warn('elm-plugin.onchange', filePath);
+    },
+  };
+};
+
+/**
+ * Transforms the IIFE the elm compiler returns to an ES module
+ * @param {string} iife - the js source code emitted from the elm compiler
+ */
+function toESM(iife) {
+  // Copied from https://github.com/ChristophP/elm-esm/blob/17e7f56aa1ec313f0ab0c47ef15142ba64fd9169/src/index.js
+  const elmExports = iife.match(/^_Platform_export\(([^]*?)\);/m)[1];
+  return iife
+    .replace(/^\(function\(scope\)\{$/m, '// -- $&')
+    .replace(/^'use strict';$/m, '// -- $&')
+    .replace(/function _Platform_export([^]*?)\n\}\n/g, '/*\n$&\n*/')
+    .replace(/function _Platform_mergeExports([^]*?)\n\}\n/g, '/*\n$&\n*/')
+    .replace(/^_Platform_export\(([^]*?);$/m, '/*\n$&\n*/')
+    .concat('\n')
+    .concat(`export const Elm = ${elmExports};\nexport default Elm;\n`);
+}
